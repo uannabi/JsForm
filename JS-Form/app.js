9fd594c56f9f640becff129af7ed46a74b144fbe
Zahid Un Nabi <zahid_sc@hotmail.fr>
2017-10-24 12:06:34 +0600

    JS Form
    Add element, delete element and filter search

---
+++ JS-Form/app.js
@@ -0,0 +1,68 @@
+const list = document.querySelector('#book-list ul');
+
+list.addEventListener('click', function(e) {
+  if(e.target.className=='delete') {
+    const li = e.target.parentElement;
+    list.removeChild(li);
+  }
+})
+
+
+
+// add books-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+
+// create element
+
+const li = document.createElement('li');
+const bookName = document.createElement('span');
+const deletBtn = document.createElement('span');
+
+//add content
+deletBtn.textContent = 'delete';
+bookName.textContent = value;
+
+//ad classes
+bookName.classList.add('name');
+deletBtn.classList.add('delete');
+
+//append to document
+
+li.appendChild(bookName);
+li.appendChild(deletBtn);
+list.appendChild(li);
+
+});
+
+//hide books
+
+const  hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change',function(e) {
+  if (hideBox.checked) {
+    list.style.display = "none";
+  }else {
+    list.style.display = "block";
+  }
+
+});
+
+
+//filter books
+
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup',function(e) {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book){
+    const title = book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) != -1){
+      book.style.display = 'block';
+    }else {
+      book.style.display = 'none';
+    }
+  })
+})
